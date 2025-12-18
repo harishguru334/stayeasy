453a6542a7ebcda37d3425e3cbdc243205028bba
Harish Guru <harishguru334>
2025-12-18 23:02:04 +0530

    Remove backend from frontend repository

--- Backend/Routes/RoomRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const { createroom, getRoom } = require("../Controller/RoomController")
-const router = express.Router()
-
-router.post('/Room', createroom)
-router.get('/get',getRoom)
-
-
-module.exports = router
